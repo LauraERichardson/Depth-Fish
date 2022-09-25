3ead617309cc1d8429965d98168545a7a1bd7e69
Philipp Neubauer <neubauer.phil@gmail.com>
2022-09-25 21:18:09 +1300

    Update Table1.R

--- figures/Table1.R
+++ figures/Table1.R
@@ -118,8 +118,8 @@ rownames(popstore_dec) <- depnms
 unpopstore
 popstore
 
-write_csv(popstore, 'table1a.csv')
-write_csv(unpopstore, 'table1b.csv')
+write_csv(as.data.frame(popstore), 'table1a.csv')
+write_csv(as.data.frame(unpopstore), 'table1b.csv')
 
 
 # could just have calculated the decrease as 1 - the probability of increase: 
