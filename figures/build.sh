bf4fb331aaa10d6eff8a4f4246c66bed0f12fa71
Philipp Neubauer <neubauer.phil@gmail.com>
2022-09-25 21:23:48 +1300

    Update build.sh

--- figures/build.sh
+++ figures/build.sh
@@ -5,7 +5,7 @@ set -ex
 dir /input/
 
 export CLOUD_RUN="TRUE"
-Rscript pop_effect_figs.R # doesn't work with new int terms in s()
+#Rscript pop_effect_figs.R # doesn't work with new int terms in s()
 Rscript depth_dens_plot.R
 Rscript slope_dens_plot.R
 Rscript proba_of_increase_plot.R
