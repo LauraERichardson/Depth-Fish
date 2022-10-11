09252a3a1eb4c66a692f0e516ba83410d1738fad
Laura E. Richardson <l.richardson@bangor.ac.uk>
2022-10-11 10:01:16 +0100

    Changing to prob of absolute not % change

--- figures/Table2.R
+++ figures/Table2.R
@@ -37,11 +37,11 @@ pp %>% group_by(trophic_group, .draw, POP_STATUS) %>%
   mutate(
     hu = ifelse(is.na(hu),0,hu),
     expect = (1-hu)*mu,
-    rat = expect/lag(expect)) %>%
+    rat = expect - lag(expect)) %>%
   filter(!is.na(rat)) %>%
   arrange(trophic_group, .draw, DEPTH,POP_STATUS) %>%
   group_by(trophic_group, .draw, DEPTH) %>%
-  summarise(rat_pop = rat[2]/rat[1]) %>% # populated over unpopulated
+  summarise(rat_pop = rat[2] - rat[1]) %>% # populated over unpopulated
   group_by(trophic_group, DEPTH) %>%
   summarise(prob=mean(rat_pop>1)) %>%  # probability that fish biomass at populated islands increases faster than at unpopulated islands
   pivot_wider(names_from = trophic_group, values_from = prob) %>%
