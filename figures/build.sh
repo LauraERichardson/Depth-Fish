9f5f008d58a342e679463ddee0a4a2226ea88464
Laura E. Richardson <l.richardson@bangor.ac.uk>
2022-11-01 11:30:25 +0000

    Run table S10

--- figures/build.sh
+++ figures/build.sh
@@ -5,16 +5,16 @@ set -ex
 dir /input/
 
 export CLOUD_RUN="TRUE"
-Rscript -e "R.version; installed.packages(fields = 'Version')[,'Version']"
+#Rscript -e "R.version; installed.packages(fields = 'Version')[,'Version']"
 #Rscript pop_effect_figs.R # doesn't work with new int terms in s()
 #Rscript depth_dens_plot.R
 #Rscript slope_dens_plot.R
 #Rscript proba_of_increase_plot.R
 #Rscript proba_of_increase_slope.R
-#Rscript ternery_plot.R
+Rscript ternery_plot.R
 #Rscript Table1.R
 #Rscript TablesS7_S8.R
-#Rscript TableS10.R
+
 
 
 if test -n "$(find . -maxdepth 1 -name '*.csv' -print -quit)"
