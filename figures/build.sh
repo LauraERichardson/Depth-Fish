8ed62ad64f497d71b9415eea6453e8ca354aa64f
Laura E. Richardson <l.richardson@bangor.ac.uk>
2023-02-06 17:02:40 +0000

    run isl_er_depth_fx plot

--- figures/build.sh
+++ figures/build.sh
@@ -11,12 +11,12 @@ export CLOUD_RUN="TRUE"
 #Rscript slope_dens_plot.R
 #Rscript proba_of_increase_plot.R
 #Rscript proba_of_increase_slope.R
-#Rscript island_ecoregion_depth_plot.R
+Rscript island_ecoregion_depth_plot.R
 #Rscript ternery_plot.R
 #Rscript Table1.R
 #Rscript TablesS7_S8.R
 #Rscript summary_tables.R
-Rscript dens_prior_post_plot.R
+#Rscript dens_prior_post_plot.R
 
 
 if test -n "$(find . -maxdepth 1 -name '*.csv' -print -quit)"
