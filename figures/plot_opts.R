3b94f7d80a16d567304507406b7e6bb290b6ae3f
Laura E. Richardson <l.richardson@bangor.ac.uk>
2023-02-13 10:04:25 +0000

    Add Pisc_Sj to plot_opts

--- figures/plot_opts.R
+++ figures/plot_opts.R
@@ -9,8 +9,8 @@ legend.fun<-function(...){
 }
 
 # plot display names 
-nms <- c("Total biomass", "Primary consumer","Planktivore","Secondary consumer","Piscivore")
+nms <- c("Total biomass", "Primary consumer","Planktivore","Secondary consumer","Piscivore","Piscivore_all")
 # colour for each model
-mycols <- c("#0072B2","#009E73","#56B4E9","#CC79A7","#E69F00")
-hurd  <- c(F,F,T,F,T)
+mycols <- c("#0072B2","#009E73","#56B4E9","#CC79A7","#E69F00","#D55E00")
+hurd  <- c(F,F,T,F,T,T)
 #desired_order <- c("Depth","Slope","Pop_Status","Depth:Pop_Status","Depth Slope smooth 1","Depth Slope smooth 2") 
