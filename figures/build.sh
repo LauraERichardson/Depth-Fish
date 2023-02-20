f8ecf46931510c0dd7fe0bb46bb8073c9788ce51
Laura E. Richardson <l.richardson@bangor.ac.uk>
2023-02-20 10:31:08 +0000

    Run slope dens plot

--- figures/build.sh
+++ figures/build.sh
@@ -7,8 +7,8 @@ dir /input/
 export CLOUD_RUN="TRUE"
 #Rscript -e "R.version; installed.packages(fields = 'Version')[,'Version']"
 #Rscript pop_effect_figs.R # doesn't work with new int terms in s()
-Rscript depth_dens_plot.R
-#Rscript slope_dens_plot.R
+#Rscript depth_dens_plot.R
+Rscript slope_dens_plot.R
 #Rscript proba_of_increase_plot.R
 #Rscript proba_of_increase_slope.R
 #Rscript island_ecoregion_depth_plot.R
