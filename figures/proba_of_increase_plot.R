713294ae37510aac9a628e6a17bbb2c4799c1ae8
Laura E. Richardson <l.richardson@bangor.ac.uk>
2023-02-20 11:10:07 +0000

    update fig 3 dims and font size

--- figures/proba_of_increase_plot.R
+++ figures/proba_of_increase_plot.R
@@ -126,15 +126,15 @@ g3 <- pp_bd %>%
   theme(
     strip.background = element_blank(),
     strip.text.x = element_blank(),
-    axis.text.x = element_text(size = 10, angle = 45, vjust = 1, hjust = 1),
-    axis.text.y = element_text(size = 10),
-    legend.text = element_text(size=10)
+    axis.text.x = element_text(size = 7, angle = 45, vjust = 1, hjust = 1),
+    axis.text.y = element_text(size = 7),
+    legend.text = element_text(size=7)
   )
 
 
 g1+g2+g3 + patchwork::plot_layout(widths=c(0.70,0.15,0.15))
 
-ggsave('Figure3_alt.pdf',device="pdf", width = 7, height = 4, units = 'in',dpi = 300)
+ggsave('Figure3_alt.pdf',device="pdf", width = 7, height = 5, units = 'in',dpi = 300)
 
 
 ######
