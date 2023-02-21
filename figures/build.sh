bd56ce1f0e2cdf6d81c3f63bef5498a79966e99d
Laura E. Richardson <l.richardson@bangor.ac.uk>
2023-02-21 14:07:25 +0000

    Run Fig S1

--- figures/build.sh
+++ figures/build.sh
@@ -6,10 +6,10 @@ dir /input/
 
 export CLOUD_RUN="TRUE"
 #Rscript -e "R.version; installed.packages(fields = 'Version')[,'Version']"
-#Rscript pop_effect_figs.R # doesn't work with new int terms in s()
+Rscript pop_effect_figs.R # doesn't work with new int terms in s()
 #Rscript depth_dens_plot.R
 #Rscript slope_dens_plot.R
-Rscript proba_of_increase_plot.R
+#Rscript proba_of_increase_plot.R
 #Rscript proba_of_increase_slope.R
 #Rscript island_ecoregion_depth_plot.R
 #Rscript ternery_plot.R
